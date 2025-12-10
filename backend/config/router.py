74d3d60374e5c82f52596c9e4f051b383f7a4b0b
Tanmay <38372175+tanmaybug@users.noreply.github.com>
2025-12-10 22:13:01 +0530

    feat: View profile service (#6)
    * profile view service and download service added
    
    * profile download html page added

--- backend/config/router.py
+++ backend/config/router.py
@@ -7,16 +7,23 @@ from controllers.V1 import (
     noc_application_controller,
     test_new,
 )
-from controllers.V1.institution import form1Controller, form2Controller,form3Controller
+
+from controllers.V1.institution import (
+    form1Controller,
+    form2Controller,
+    form3Controller,
+    view_download_application_controller,
+)
 
 master_router = APIRouter()
 
-master_router.include_router(registration_controller.router,prefix="/v1")
+master_router.include_router(registration_controller.router, prefix="/v1")
 master_router.include_router(login_controller.router, prefix="/v1")
 master_router.include_router(noc_application_controller.router, prefix="/v1")
 
 master_router.include_router(test_new.router, prefix="/v1")
 master_router.include_router(otp_controller.router, prefix="/v1")
+master_router.include_router(view_download_application_controller.router, prefix="/v1")
 
 master_router.include_router(form1Controller.router, prefix="/v1")
 master_router.include_router(form2Controller.router, prefix="/v1")
